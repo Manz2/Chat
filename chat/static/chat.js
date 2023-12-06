d200dd4e138952b454a7673753e5fc841add145d
Manz2 <erikmanzer21@gmail.com>
2023-12-06 12:40:39 +0100

    java script

--- chat/static/chat.js
+++ chat/static/chat.js
@@ -1,22 +1,34 @@
 window.onload = function() {
+    messages = [];
     chat();
 };
 
-  var messages = [
-    {text : "Malcom",sender:"me"},
-    {text : "Kaylee",sender:"you"},
-    {text : "Jayne",sender:"me"},
-  ];
-
 function chat() {
     var result = "";
-    messages.forEach(function(item){
-        if(item.sender == "me"){
+    messages.forEach(function(item) {
+        if (item.sender == "me") {
             result += "<div id=\"message\">" + item.text + "</div>" + "<br></br>";
         } else {
             result += "<div id=\"messageF\">" + item.text + "</div>" + "<br></br>";
         }
     });
 
-    document.getElementById("messages").innerHTML = result
+    document.getElementById("messages").innerHTML = result;
 }
+
+function handleClick(event) {
+    var messageInput = document.getElementById("messageForm");
+    var messageText = messageInput.value;
+
+    if (!Array.isArray(messages)) {
+        messages = [];
+    }
+    messages.push({ text: messageText, sender: "me" });
+
+    chat();
+    messageInput.value = "";
+    pane.scrollTop = pane.scrollHeight;
+
+    event.preventDefault();
+}
+
